741a7346413652c238338c4f48557817350b8f60
BolleyB <yeahbolley@gmail.com>
2024-05-14 18:13:47 -0400

    implemented sidebad panel with responsive layout for mobile and web browser. Also implemented a pie chart and counter for bank accounts.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import Sidebar from "@/components/ui/Sidebar";
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Hugo', lastName: 'Webster'}
   return (
-    <main>
-      SIDEBAR 
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn}/>
+      <div className="flex size-full">
+
+      </div>
       {children}  
     </main>
   );
